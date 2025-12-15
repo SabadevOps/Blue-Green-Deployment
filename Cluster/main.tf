7a1fe745e1b0246e872162722f7af18886dee687
Saba N <159530700+SabadevOps@users.noreply.github.com>
2025-12-15 15:50:44 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -1,7 +1,11 @@
+```hcl
 provider "aws" {
   region = "ap-south-1"
 }
 
+########################
+# VPC
+########################
 resource "aws_vpc" "devopsshack_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -10,8 +14,11 @@ resource "aws_vpc" "devopsshack_vpc" {
   }
 }
 
+########################
+# Subnets (2 AZs)
+########################
 resource "aws_subnet" "devopsshack_subnet" {
-  count = 2
+  count                   = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
@@ -22,6 +29,9 @@ resource "aws_subnet" "devopsshack_subnet" {
   }
 }
 
+########################
+# Internet Gateway
+########################
 resource "aws_internet_gateway" "devopsshack_igw" {
   vpc_id = aws_vpc.devopsshack_vpc.id
 
@@ -30,6 +40,9 @@ resource "aws_internet_gateway" "devopsshack_igw" {
   }
 }
 
+########################
+# Route Table + Association
+########################
 resource "aws_route_table" "devopsshack_route_table" {
   vpc_id = aws_vpc.devopsshack_vpc.id
 
@@ -49,6 +62,9 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.devopsshack_route_table.id
 }
 
+########################
+# Security Groups
+########################
 resource "aws_security_group" "devopsshack_cluster_sg" {
   vpc_id = aws_vpc.devopsshack_vpc.id
 
@@ -86,36 +102,9 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
-  }
-}
-
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  instance_types = ["t2.large"]
-
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
-  }
-}
-
+########################
+# IAM Roles
+########################
 resource "aws_iam_role" "devopsshack_cluster_role" {
   name = "devopsshack-cluster-role"
 
@@ -173,3 +162,51 @@ resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_polic
   role       = aws_iam_role.devopsshack_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+
+########################
+# EKS Cluster
+########################
+resource "aws_eks_cluster" "devopsshack" {
+  name     = "devopsshack-cluster"
+  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
+    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+  }
+}
+
+########################
+# EKS Managed Node Group
+########################
+resource "aws_eks_node_group" "devopsshack" {
+  cluster_name    = aws_eks_cluster.devopsshack.name
+  node_group_name = "devopsshack-node-group"
+  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
+  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  instance_types = ["c7i-flex.large"]
+
+  remote_access {
+    ec2_ssh_key               = "kuku_Mumbai"
+    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+  }
+}
+
+########################
+# Outputs (optional)
+########################
+output "cluster_name" {
+  value = aws_eks_cluster.devopsshack.name
+}
+
+output "subnet_ids" {
+  value = aws_subnet.devopsshack_subnet[*].id
+}
+```
